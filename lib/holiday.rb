9c7781805573c7b2ebc841885f94079c0f3c137b
Pat McClernan <patmccler@gmail.com>
2020-05-01 14:58:39 -0400

    tests passing

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -25,6 +26,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
@@ -32,16 +35,16 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].collect {|holiday, supply_array| supply_array }.flatten
 
 end
 
@@ -54,12 +57,28 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+    holidays.each do |holiday, supply_array|
+      puts "  #{titlize holiday}: #{supply_array.map{|supply| titlize supply}.join(', ')}"
+    end
+  end
+end
+
+def titlize string
+  if string == string.upcase
+    string
+  else
+    string.to_s.gsub("_"," ").split.map(&:capitalize).join(" ")
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holiday_hash.inject([]) do |season_memo, (season, holidays)|
+    season_memo << holidays.inject([]) do |holiday_memo, (holiday, supply_array)|
+      (supply_array.include? "BBQ") ? (holiday_memo << holiday) : holiday_memo
+    end
+  end.flatten
 end
 
 
